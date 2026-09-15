3449ac9515fe6dafc3a2408bd0d16464233226c6
Fome <110628560+fomecrazy@users.noreply.github.com>
2026-09-15 11:56:20 +0200

    fix:  type declarations

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-declare function $migrate(order: number): never;
-declare function $migrate(order: "__resolved"): number;
-declare function $migrate(order: number | "__resolved"): number?;
+export declare function $migrate(order: number): never;
+export declare function $migrate(order: "__resolved"): number;
+export declare function $migrate(order: number | "__resolved"): number | undefined;
