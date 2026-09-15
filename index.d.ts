b234d8667e2e91e1502285fadcfdc2c0439e9a65
Fome <110628560+fomecrazy@users.noreply.github.com>
2026-09-15 19:26:46 +0200

    revert: useless type change

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 export declare function $migrate(order: number): never;
-export declare function $migrate(order: "__resolved", path: string[]): number;
-export declare function $migrate(order: number | "__resolved", path: string[]): number | undefined;
+export declare function $migrate(order: "__resolved"): number;
+export declare function $migrate(order: number | "__resolved"): number | undefined;
